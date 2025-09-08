ed8c3f7a8cc5943f1ce2f9f26efc4d3a19a5d563
Tosapon Waikitja <tonspu55@gmail.com>
2025-09-09 00:12:09 +0700

    update env

--- nextjs-campnaidee/src/sanity/client.ts
+++ nextjs-campnaidee/src/sanity/client.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 export const client = createClient({
   projectId: "tqzf3jx1",
-  dataset: "production",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2024-01-01",
   useCdn: false,
   token: process.env.SANITY_API_TOKEN,
