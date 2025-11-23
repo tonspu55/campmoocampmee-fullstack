51fc5b8ab2ff805ee1d769b55104d70ce9ea93d0
Tosapon Waikitja <tonspu55@gmail.com>
2025-11-23 14:50:27 +0700

    upgrade to nextjs version 16

--- nextjs-campnaidee/next.config.ts
+++ nextjs-campnaidee/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: "/Users/tosaponwaikitja/Desktop/freelance/campnaidee-fullstack/nextjs-campnaidee",
+  },
   images: {
     remotePatterns: [
       {
