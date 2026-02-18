bd865244d98d17e8a619dff6569f649a3f649f18
Tosapon Waikitja <tonspu55@gmail.com>
2026-02-18 22:18:21 +0700

    update name

--- nextjs-campmoocampmee/next.config.ts
+++ nextjs-campmoocampmee/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   turbopack: {
-    root: "/Users/tosaponwaikitja/Desktop/freelance/campnaidee-fullstack/nextjs-campnaidee",
+    root: "/Users/tosaponwaikitja/Desktop/freelance/campmoocampmee-fullstack/nextjs-campmoocampmee",
   },
   images: {
     remotePatterns: [
